e0dcdf52a0101b47f810dae437f8d5a5d5b75138
nazgulrabbit <maxsermi@gmail.com>
2017-11-22 23:26:47 +0100

    code to create an array of images

---
+++ app.js
@@ -0,0 +1,10 @@
+// all the scripts for the memory game are here
+
+// code to create an array that loops the images
+var  tileImage = [];
+for (var i = 0; i < 9; i++){
+tileImage.push(i + ".png");
+
+}
+
+console.log(tileImage);
