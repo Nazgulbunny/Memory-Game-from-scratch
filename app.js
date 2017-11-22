57807643065a4be18c10853fd3d78d77925652c7
nazgulrabbit <maxsermi@gmail.com>
2017-11-23 00:52:37 +0100

    corrected errore in eventlistener code

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // all the scripts for the memory game are here
+
 let tileImage = [];
+let tileArray =[];
 let startButton = document.getElementById("start");
+let gamePlay = false;
 
 
 //event listens
 
 // code to create an array that loops the images
 
-startButton = addEventListener("click",startGame);
+startButton.addEventListener("click",startGame);
 
 for (var i = 1; i < 9; i++){
 tileImage.push(i + ".png");
@@ -24,7 +27,7 @@ tileImage.push(i + ".png");
 //functions
 
 function startGame (){
-startButton.style.display='none';
+startButton.style.display ='none';
 console.log("started");
 
 }
