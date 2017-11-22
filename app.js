43bc1857437e949a981472ef91e562b1cb921089
nazgulrabbit <maxsermi@gmail.com>
2017-11-23 00:17:10 +0100

    added event listener and function to start the game

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 // all the scripts for the memory game are here
+let tileImage = [];
+let startButton = document.getElementById("start");
+
+
+//event listens
 
 // code to create an array that loops the images
-var  tileImage = [];
+
+startButton = addEventListener("click",startGame);
+
 for (var i = 1; i < 9; i++){
 tileImage.push(i + ".png");
 
 }
 
-console.log(tileImage);
+
+
+
+
+
+
+
+//functions
+
+function startGame (){
+startButton.style.display='none';
+console.log("started");
+
+}
