9d94e1f3750c3d4012cf74da0557e2785033bbd4
nazgulrabbit <maxsermi@gmail.com>
2017-11-23 23:52:19 +0100

    added function to match cards

--- app.js
+++ app.js
@@ -1,59 +1,32 @@
 // all the scripts for the memory game are here
 
-let tileImage = [];
-let tileArray =[];
-let startButton = document.getElementById("start");
-let gamePlay = false;
-let gameBoard = document.getElementById("gameboard");
-let cardFlipped = -1;
+var tileImages = [];
+    var tileArray = [];
+    var tileFlippedOver = [];
+    var cardFlipped = -1;
+    var timer = '';
+    var playLockout = false;
+    var startButton = document.getElementById('start');
+    var gameBoard = document.getElementById('gameboard');
+    var gamePlay = false;
 
-//event listens
+    //event listens
+    startButton.addEventListener('click', startGame);
 
-startButton.addEventListener("click",startGame);
+    //Functions
+    function startGame() {
+      startButton.style.display = 'none';
+      if (!gamePlay) {
+        gamePlay = true;
+        buildArray();
+        tileArray = tileImages.concat(tileImages);
+        shuffleArray(tileArray);
+        buildBoard();
 
+      }
 
+    }
 
-
-
-
-
-
-
-//functions
-
-function startGame (){
-startButton.style.display ='none';
-if (!gamePlay) {
-	gamePlay=true;
-	buildArray();
-	tileArray=tileImage.concat(tileImage);
-	shuffleArray(tileArray);
-	buildBoard()
-}
-
-
-}
-
-//function to build the array
-function buildArray(){
-	for (var i = 1; i < 9; i++){
-tileImage.push(i + ".png");
-
-}
-}
-
-function shuffleArray(array){
-
-	for (var i = array.length - 1; i >= 0; i--) {
-		var holder = Math.floor(Math.random()* (i + 1));
-		var itemValue = array[i];
-		array[i] = array[holder];
-		array[holder]=itemValue;
-	}
-	return array;
-}
-
-//function to build the board
     function buildBoard() {
       var html = "";
       for (var x = 0; x <= (tileArray.length - 1); x++) {
@@ -63,28 +36,75 @@ function shuffleArray(array){
       gameBoard.innerHTML = html;
     }
 
+    function isinArray(v, array) {
+      return array.indexOf(v) > -1;
+    }
 
-  // function to be able to pick up a card
-  
-    function pickCard(tileIndex, t) {
-      console.log(tileIndex);
-      console.log(cardFlipped);
-      console.log(t.src);
-      // check if its already flipped
-      if (cardFlipped >= 0) {
-        //second cardFlipped
-        if (tileIndex != cardFlipped) {
-          // second card new
-          t.src = "images/" + tileArray[tileIndex];
-          var secondCard = tileIndex;
-          // check for match
+    function cardFlip(t, ti) {
 
-        }
-        console.log('second', cardFlipped);
-      } else {
-        //first cardFlipped
-        cardFlipped = tileIndex;
-        t.src = "images/" + tileArray[tileIndex];
-        console.log('first', cardFlipped);
+      t.src = "images/" + tileArray[ti];
+      tileFlippedOver.push(t.id);
+      console.log(tileFlippedOver);
+    }
+
+    function hideCard() {
+      for (var x = 0; x < 2; x++) {
+        var vid = tileFlippedOver.pop();
+        document.getElementById(vid).src = "images/back.png";
+        console.log(vid);
       }
-    } 
+      clearInterval(timer);
+      playLockout = false;
+      cardFlipped = -1;
+    }
+
+    function checkSrc(v) {
+      var v = document.getElementById(v).src;
+      return v;
+    }
+
+    function pickCard(tileIndex, t) {
+      // check if its already flipped
+
+      if (!isinArray(t.id, tileFlippedOver) && !playLockout) {
+        console.log('not in array');
+        if (cardFlipped >= 0) {
+          cardFlip(t, tileIndex);
+          var secondCard = tileIndex;
+          playLockout = true;
+          if (checkSrc(tileFlippedOver[tileFlippedOver.length - 1]) == checkSrc(tileFlippedOver[tileFlippedOver.length - 2])) {
+            //Match
+            console.log('Match');
+            playLockout = false;
+            cardFlipped = -1;
+          } else {
+            //No Match
+            console.log('No Match');
+            timer = setInterval(hideCard, 3000);
+
+          }
+        } else {
+          cardFlipped = tileIndex;
+          cardFlip(t, tileIndex);
+        }
+
+      } else {
+        console.log('in array');
+      }
+    }
+
+    function buildArray() {
+      for (var x = 1; x < 9; x++) {
+        tileImages.push(x + '.png');
+      }
+    }
+
+    function shuffleArray(array) {
+      for (var x = array.length - 1; x > 0; x--) {
+        var holder = Math.floor(Math.random() * (x + 1));
+        var itemValue = array[x];
+        array[x] = array[holder];
+        array[holder] = itemValue;
+      }
+      return array;
+    }
