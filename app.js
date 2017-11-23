d7cb6d58725ae5690e5ff4d514800c2f8392e643
nazgulrabbit <maxsermi@gmail.com>
2017-11-23 16:48:12 +0100

    Math function added

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ let gamePlay = false;
 
 startButton.addEventListener("click",startGame);
 
-for (var i = 1; i < 9; i++){
-tileImage.push(i + ".png");
 
-}
 
 
 
@@ -28,6 +25,32 @@ tileImage.push(i + ".png");
 
 function startGame (){
 startButton.style.display ='none';
+if (!gamePlay) {
+	gamePlay=true;
+	buildArray();
+	tileArray=tileImage.concat(tileImage);
+	shuffleArray(tileArray);
+	console.log(tileArray);
+}
 console.log("started");
 
+}
+
+//function to build the array
+function buildArray(){
+	for (var i = 1; i < 9; i++){
+tileImage.push(i + ".png");
+
+}
+}
+
+function shuffleArray(array){
+
+	for (var i = array.length - 1; i >= 0; i--) {
+		var holder = Math.floor(Math.random()* (i + 1));
+		var itemValue = array[i];
+		array[i] = array[holder];
+		array[holder]=itemValue;
+	}
+	return array;
 }
